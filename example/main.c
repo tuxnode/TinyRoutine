3b0c958b1d96c8228734f186e03e0b38f2ed3db4
Tux Node <tuxnode@outlook.com>
2026-05-22 09:46:07 +0800

    format: linux C style

--- example/main.c
+++ example/main.c
@@ -1,30 +1,33 @@
-#include <stdio.h>
 #include "uthread.h"
+#include <stdio.h>
 
-void foo(void) {
-    for (int i = 0; i < 5; i++) {
-        printf("  [foo] iteration %d\n", i);
-        uthread_yield();
-    }
-    printf("  [foo] done\n");
+void foo(void)
+{
+	for (int i = 0; i < 5; i++) {
+		printf("  [foo] iteration %d\n", i);
+		uthread_yield();
+	}
+	printf("  [foo] done\n");
 }
 
-void bar(void) {
-    for (int i = 0; i < 3; i++) {
-        printf("  [bar] iteration %d\n", i);
-        uthread_yield();
-    }
-    printf("  [bar] done\n");
+void bar(void)
+{
+	for (int i = 0; i < 3; i++) {
+		printf("  [bar] iteration %d\n", i);
+		uthread_yield();
+	}
+	printf("  [bar] done\n");
 }
 
-int main(void) {
-    int t0 = uthread_creat(foo);
-    int t1 = uthread_creat(bar);
+int main(void)
+{
+	int t0 = uthread_creat(foo);
+	int t1 = uthread_creat(bar);
 
-    printf("[main] created thread %d and %d, starting scheduler\n", t0, t1);
+	printf("[main] created thread %d and %d, starting scheduler\n", t0, t1);
 
-    schedule();
+	schedule();
 
-    printf("[main] back to main, done\n");
-    return 0;
+	printf("[main] back to main, done\n");
+	return 0;
 }
--- include/uthread.h
+++ include/uthread.h
@@ -7,27 +7,27 @@
 #define MAX_THREADS 4
 
 typedef struct {
-  uint64_t rsp;
-  uint64_t rbp;
-  uint64_t rbx;
-  uint64_t r12;
-  uint64_t r13;
-  uint64_t r14;
-  uint64_t r15;
-}context_t;
+	uint64_t rsp;
+	uint64_t rbp;
+	uint64_t rbx;
+	uint64_t r12;
+	uint64_t r13;
+	uint64_t r14;
+	uint64_t r15;
+} context_t;
 
 typedef enum {
-  FREE,
-  READY,
-  RUNNING,
-  EXITED,
-}ThreadState;
+	FREE,
+	READY,
+	RUNNING,
+	EXITED,
+} ThreadState;
 
 typedef struct {
-  int id;
-  context_t ctx;
-  ThreadState state;
-  char *stack;
+	int id;
+	context_t ctx;
+	ThreadState state;
+	char *stack;
 } TCB;
 
 // switch.S
